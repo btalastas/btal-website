390d1003f5dd8f9477845739379bd55f44d33244
bjornhehe <talastasbjorn@gmail.com>
2024-03-10 19:16:03 -0400

    changed tab views for mobile. Mapped tab names instead of hard coding.

--- src/components/CenteredTabs.js
+++ src/components/CenteredTabs.js
@@ -2,15 +2,33 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function CenteredTabs({ value, onChange }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const tabNames = ["About me", "School", "Hobbies and interests", "Resume"];
+
   return (
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <Tabs value={value} onChange={onChange} centered>
-        <Tab label="About me" />
+      <Tabs
+        value={value}
+        onChange={onChange}
+        centered={!isMobile} // Center tabs on larger screens
+        variant={isMobile ? "scrollable" : undefined} // Make tabs scrollable on smaller screens
+        scrollButtons={isMobile ? "auto" : false} // Auto show scroll buttons on small screens
+        allowScrollButtonsMobile
+        aria-label="scrollable centered tabs example"
+      >
+        {tabNames.map((name, index) => (
+          <Tab label={name} key={index} />
+        ))}
+        {/* <Tab label="About me" />
         <Tab label="School" />
         <Tab label="Hobbies and interests" />
-        <Tab label="Resume" />
+        <Tab label="Resume" /> */}
       </Tabs>
     </Box>
   );
