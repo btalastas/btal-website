5a5c141decf4f0dcd23108a954b8619c6899c6ab
bjornhehe <talastasbjorn@gmail.com>
2024-03-11 01:15:41 -0400

    fixed warnings

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,6 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../assets/documents/Talastas_Bjorn_Resume.pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Resume() {
   return (
