3812187f927f1a4338db6da531cb7baebfcb4be0
bjornhehe <talastasbjorn@gmail.com>
2024-04-24 13:14:46 -0400

    updated contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,52 @@
-import { Container, Typography } from "@mui/material";
+import { Email, GitHub, LinkedIn } from "@mui/icons-material";
+import {
+  Box,
+  Container,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+} from "@mui/material";
 
 export default function Contact() {
   return (
     <Container>
-      <Typography variant="h3" component="div">
-        Contact Me
-      </Typography>
-      <Typography varient="body1"> At these locations</Typography>
+      <Box color="#cfe8fc">
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
+          color="#cfe8fc"
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <Email color="primary" />
+                </ListItemIcon>
+                <Link href="mailto:btalasta@gmu.edu">Email me</Link>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <LinkedIn color="primary" />
+                </ListItemIcon>
+                <Link href="https://www.linkedin.com/in/btalastas/">
+                  Linkedin
+                </Link>
+              </ListItem>
+
+              <ListItem>
+                <ListItemIcon>
+                  <GitHub color="primary" />
+                </ListItemIcon>
+                <Link href="https://github.com/btalastas">Github</Link>
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 }
