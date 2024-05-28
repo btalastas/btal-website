9904c98fa50fb3b9baf128441e796d1f302410b2
Bjorn Talastas <talastasbjorn@gmail.com>
2024-05-28 14:03:33 -0400

    mobile viewing purposes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,34 +1,30 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import MyTimeline from "./MyTimeline";
 
 export default function AboutMe() {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "flex-start",
-        gap: 2,
-        mt: 4,
-        mb: 4,
-      }}
-    >
-      <Box sx={{ flex: 1, boxShadow: 10 }}>
-        <Typography variant="h2">Bjorn N. Talastas</Typography>
-        <Typography paragraph>
-          First gen american with Filipino descent. Lorem Ipsem
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          flex: 1,
-          borderLeft: 2,
-          p: { xs: 1, sm: 2, md: 3 },
-          boxShadow: 10,
-        }}
-      >
-        <MyTimeline />
-      </Box>
+    <Container sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={2} alignItems="flex-start">
+        <Grid item xs={12} md={6}>
+          <Box sx={{ boxShadow: 10, p: 2 }}>
+            <Typography variant="h2">Bjorn N. Talastas</Typography>
+            <Typography paragraph>
+              First gen american with Filipino descent. Lorem Ipsum
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              borderLeft: { md: 2, xs: 0 },
+              boxShadow: 10,
+              p: { xs: 1, sm: 2, md: 3 },
+            }}
+          >
+            <MyTimeline />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
